2b3566572eda122698fe6d5882de97e67d105a39
fresky <dawei.xu@gmail.com>
2013-07-19 20:21:45 +0800

    support highlight code

--- MarkdownInLiveWriter/MarkdownForm.Designer.cs
+++ MarkdownInLiveWriter/MarkdownForm.Designer.cs
@@ -31,10 +31,17 @@
             this.markdownTextBox = new System.Windows.Forms.TextBox();
             this.htmlTabControl = new System.Windows.Forms.TabControl();
             this.PreviewPage = new System.Windows.Forms.TabPage();
-            this.SourcePage = new System.Windows.Forms.TabPage();
             this.previewWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.SourcePage = new System.Windows.Forms.TabPage();
             this.sourceTextBox = new System.Windows.Forms.TextBox();
             this.insertBtn = new System.Windows.Forms.Button();
+            this.highlightBtn = new System.Windows.Forms.Button();
+            this.highlightCheckbox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.languageComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.styleComboBox = new System.Windows.Forms.ComboBox();
+            this.showLineCheckBox = new System.Windows.Forms.CheckBox();
             this.htmlTabControl.SuspendLayout();
             this.PreviewPage.SuspendLayout();
             this.SourcePage.SuspendLayout();
@@ -77,17 +84,6 @@
             this.PreviewPage.Text = "Preview";
             this.PreviewPage.UseVisualStyleBackColor = true;
             // 
-            // SourcePage
-            // 
-            this.SourcePage.Controls.Add(this.sourceTextBox);
-            this.SourcePage.Location = new System.Drawing.Point(4, 4);
-            this.SourcePage.Name = "SourcePage";
-            this.SourcePage.Padding = new System.Windows.Forms.Padding(3);
-            this.SourcePage.Size = new System.Drawing.Size(461, 540);
-            this.SourcePage.TabIndex = 1;
-            this.SourcePage.Text = "Source";
-            this.SourcePage.UseVisualStyleBackColor = true;
-            // 
             // previewWebBrowser
             // 
             this.previewWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -97,6 +93,17 @@
             this.previewWebBrowser.Size = new System.Drawing.Size(455, 651);
             this.previewWebBrowser.TabIndex = 0;
             // 
+            // SourcePage
+            // 
+            this.SourcePage.Controls.Add(this.sourceTextBox);
+            this.SourcePage.Location = new System.Drawing.Point(4, 4);
+            this.SourcePage.Name = "SourcePage";
+            this.SourcePage.Padding = new System.Windows.Forms.Padding(3);
+            this.SourcePage.Size = new System.Drawing.Size(461, 657);
+            this.SourcePage.TabIndex = 1;
+            this.SourcePage.Text = "Source";
+            this.SourcePage.UseVisualStyleBackColor = true;
+            // 
             // sourceTextBox
             // 
             this.sourceTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -104,12 +111,12 @@
             this.sourceTextBox.Multiline = true;
             this.sourceTextBox.Name = "sourceTextBox";
             this.sourceTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.sourceTextBox.Size = new System.Drawing.Size(455, 534);
+            this.sourceTextBox.Size = new System.Drawing.Size(455, 651);
             this.sourceTextBox.TabIndex = 0;
             // 
             // insertBtn
             // 
-            this.insertBtn.Location = new System.Drawing.Point(12, 6);
+            this.insertBtn.Location = new System.Drawing.Point(12, 5);
             this.insertBtn.Name = "insertBtn";
             this.insertBtn.Size = new System.Drawing.Size(75, 23);
             this.insertBtn.TabIndex = 2;
@@ -117,16 +124,93 @@
             this.insertBtn.UseVisualStyleBackColor = true;
             this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
             // 
+            // highlightBtn
+            // 
+            this.highlightBtn.Location = new System.Drawing.Point(914, 6);
+            this.highlightBtn.Name = "highlightBtn";
+            this.highlightBtn.Size = new System.Drawing.Size(75, 23);
+            this.highlightBtn.TabIndex = 3;
+            this.highlightBtn.Text = "Highlight";
+            this.highlightBtn.UseVisualStyleBackColor = true;
+            this.highlightBtn.Click += new System.EventHandler(this.highlightBtn_Click);
+            // 
+            // highlightCheckbox
+            // 
+            this.highlightCheckbox.AutoSize = true;
+            this.highlightCheckbox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.highlightCheckbox.Checked = true;
+            this.highlightCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.highlightCheckbox.Location = new System.Drawing.Point(93, 5);
+            this.highlightCheckbox.Name = "highlightCheckbox";
+            this.highlightCheckbox.Size = new System.Drawing.Size(101, 17);
+            this.highlightCheckbox.TabIndex = 4;
+            this.highlightCheckbox.Text = "Highlight Code?";
+            this.highlightCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Language:";
+            // 
+            // languageComboBox
+            // 
+            this.languageComboBox.FormattingEnabled = true;
+            this.languageComboBox.Location = new System.Drawing.Point(264, 5);
+            this.languageComboBox.Name = "languageComboBox";
+            this.languageComboBox.Size = new System.Drawing.Size(257, 21);
+            this.languageComboBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(528, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Style:";
+            // 
+            // styleComboBox
+            // 
+            this.styleComboBox.FormattingEnabled = true;
+            this.styleComboBox.Location = new System.Drawing.Point(567, 7);
+            this.styleComboBox.Name = "styleComboBox";
+            this.styleComboBox.Size = new System.Drawing.Size(205, 21);
+            this.styleComboBox.TabIndex = 8;
+            // 
+            // showLineCheckBox
+            // 
+            this.showLineCheckBox.AutoSize = true;
+            this.showLineCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.showLineCheckBox.Checked = true;
+            this.showLineCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.showLineCheckBox.Location = new System.Drawing.Point(778, 8);
+            this.showLineCheckBox.Name = "showLineCheckBox";
+            this.showLineCheckBox.Size = new System.Drawing.Size(116, 17);
+            this.showLineCheckBox.TabIndex = 9;
+            this.showLineCheckBox.Text = "Show Line Number";
+            this.showLineCheckBox.UseVisualStyleBackColor = true;
+            // 
             // MarkdownForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.showLineCheckBox);
+            this.Controls.Add(this.styleComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.languageComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.highlightCheckbox);
+            this.Controls.Add(this.highlightBtn);
             this.Controls.Add(this.insertBtn);
             this.Controls.Add(this.htmlTabControl);
             this.Controls.Add(this.markdownTextBox);
             this.Name = "MarkdownForm";
-            this.Text = "MarkdownForm";
+            this.Text = "Markdown In Windows Live Writer";
             this.htmlTabControl.ResumeLayout(false);
             this.PreviewPage.ResumeLayout(false);
             this.SourcePage.ResumeLayout(false);
@@ -145,5 +229,12 @@
         private System.Windows.Forms.TabPage SourcePage;
         private System.Windows.Forms.TextBox sourceTextBox;
         private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.Button highlightBtn;
+        private System.Windows.Forms.CheckBox highlightCheckbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox languageComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox styleComboBox;
+        private System.Windows.Forms.CheckBox showLineCheckBox;
     }
 }
--- MarkdownInLiveWriter/MarkdownForm.cs
+++ MarkdownInLiveWriter/MarkdownForm.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
 using System.Windows.Forms;
 using MarkdownSharp;
-using Timer = System.Threading.Timer;
+using Timer = System.Windows.Forms.Timer;
+
 
 namespace MarkdownInLiveWriter
 {
     public partial class MarkdownForm : Form
     {
-        private string m_HtmlCode;
         private readonly Timer m_Timer;
         private string m_MarddownText = string.Empty;
         readonly Markdown m_Markdown = new Markdown();
@@ -15,31 +18,151 @@ namespace MarkdownInLiveWriter
         public MarkdownForm()
         {
             InitializeComponent();
-            m_Timer = new Timer(preview, null, 1000, -1);
+            m_Timer = new Timer();
+            m_Timer.Interval = 500;
+            m_Timer.Tick += preview;
+            m_Timer.Start();
+            initLanguage();
+
+            initStyle();
         }
 
-        private void preview(object state)
+        private void initStyle()
+        {
+            styleComboBox.Items.Clear();
+            styleComboBox.Items.Add("vs");
+            styleComboBox.Items.Add("colorful");
+            styleComboBox.Items.Add("emacs");
+            styleComboBox.Items.Add("vim");
+            loadFromConfig(styleComboBox, @"Plugins\CodeInLiveWriterStyle.txt");
+
+            styleComboBox.SelectedIndex = 0;
+        }
+
+        private void initLanguage()
+        {
+            languageComboBox.Items.Clear();
+            languageComboBox.Items.Add("c#");
+            languageComboBox.Items.Add("c++");
+            languageComboBox.Items.Add("java");
+            languageComboBox.Items.Add("python");
+            languageComboBox.Items.Add("ruby");
+            languageComboBox.Items.Add("c");
+            languageComboBox.Items.Add("sql");
+            languageComboBox.Items.Add("html");
+            languageComboBox.Items.Add("xml");
+            languageComboBox.Items.Add("bat");
+            languageComboBox.Items.Add("bash");
+            languageComboBox.Items.Add("powershell");
+            languageComboBox.Items.Add("registry");
+
+            loadFromConfig(languageComboBox, @"Plugins\CodeInLiveWriterLanguage.txt");
+
+            languageComboBox.SelectedIndex = 0;
+        }
+
+        private void loadFromConfig(ComboBox comboBoxStyle, string config)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(config))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string language = sr.ReadLine().Trim();
+                        if (!comboBoxStyle.Items.Contains(language))
+                        {
+                            comboBoxStyle.Items.Add(language);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void preview(object state, EventArgs eventArgs)
         {
             if (!m_MarddownText.Equals(markdownTextBox.Text))
             {
                 m_MarddownText = markdownTextBox.Text;
 
-                m_HtmlCode = m_Markdown.Transform(m_MarddownText);
-                previewWebBrowser.DocumentText = m_HtmlCode;
-                sourceTextBox.Text = m_HtmlCode;
+                previewWebBrowser.DocumentText = m_Markdown.Transform(m_MarddownText);
+                sourceTextBox.Text = previewWebBrowser.DocumentText;
             }
-            m_Timer.Change(100, 0);
         }
 
         public string GetHtmlCode()
         {
-            return m_HtmlCode;
+            return sourceTextBox.Text;
         }
 
         private void insertBtn_Click(object sender, EventArgs e)
         {
+            m_Timer.Stop();
+            highlightCode();
             DialogResult = DialogResult.OK;
-            m_Timer.Dispose();
+        }
+
+        private void highlightBtn_Click(object sender, EventArgs e)
+        {
+            highlightCode();
+        }
+
+        private void highlightCode()
+        {
+            if (highlightCheckbox.Checked)
+            {
+                foreach (HtmlElement element in previewWebBrowser.Document.GetElementsByTagName("code"))
+                {
+                    element.OuterHtml = getHtmlStyleCode(
+                        languageComboBox
+                            .SelectedItem
+                            .ToString(),
+                        element.InnerText,
+                        showLineCheckBox.Checked,
+                        styleComboBox
+                            .SelectedItem
+                            .ToString());
+                }
+                sourceTextBox.Text = previewWebBrowser.Document.Body.InnerHtml;
+            }
+        }
+
+
+        private string getHtmlStyleCode(string language, string code, bool showline, string style)
+        {
+            try
+            {
+                HttpWebRequest httpWebRequest = WebRequest.Create("http://hilite.me/api") as HttpWebRequest;
+                httpWebRequest.Method = "POST";
+                httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+
+                string parameters = string.Format("lexer={0}&code={1}&{2}&style={3}",
+                                                  HttpUtility.UrlEncode(language),
+                                                  HttpUtility.UrlEncode(code),
+                                                  showline ? "&linenos=1" : "",
+                                                  style);
+
+                using (StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(parameters);
+                }
+                string result;
+                using (WebResponse response = httpWebRequest.GetResponse())
+                {
+                    using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        result = streamReader.ReadToEnd();
+                    }
+                }
+                return result;
+            }
+            catch
+            {
+                return code;
+            }
         }
     }
 }
